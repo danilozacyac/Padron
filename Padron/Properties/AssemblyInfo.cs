bc556874e6153cdd8d10c3ab4782a367c8a2821e
Danilo <danilozacyac@gmail.com>
2018-04-22 21:47:02 -0500

    Version 3.0.0.70

--- Padron/Properties/AssemblyInfo.cs
+++ Padron/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2017.02.22066")]
-[assembly: AssemblyFileVersion("3.2017.02.22066")]
+[assembly: AssemblyVersion("3.2018.04.22066")]
+[assembly: AssemblyFileVersion("3.2018.04.22066")]
